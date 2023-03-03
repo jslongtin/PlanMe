c5a8b465a8d12bf5e9e07b44682f147084a1dac7
jslongtin <Jessikalongtin@hotmail.com>
2023-03-03 09:53:53 -0500

    arguments works por utilisateur

--- dev/PlanMe/src/Module.jsx
+++ dev/PlanMe/src/Module.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
+// TODO
 // class function
 function Module() {
     return <h2>slayyyyyyyyyy</h2>;
