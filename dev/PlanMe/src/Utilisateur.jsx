9c2470506d4e9883c0a87625a8bac3eac5c46e1a
jslongtin <Jessikalongtin@hotmail.com>
2023-03-03 11:31:00 -0500

    installer express et pgsql , liaiason a la bd a faire

--- dev/PlanMe/src/Utilisateur.jsx
+++ dev/PlanMe/src/Utilisateur.jsx
@@ -4,19 +4,68 @@ import './App.css'
 
 
 // creation utilisateur aevc argument et affichage
+// ref: w3school react pages
 function Utilisateur(props) {
     let id = props.id
-    let prenom = props.prenom
-    let nom = props.nom
-    let courriel = props.courriel
-    let motDePasse = props.motDePasse
-    let theme = props.theme
-    let photoDeProfil = props.photoDeProfil
-    let contacts = props.contacts
+    let [prenom, setPrenom] = useState(props.prenom)
+    let [nom, setNom] = useState(props.nom)
+    let [courriel, setCourriel] = useState(props.courriel)
+    let [motDePasse, setMotDePasse] = useState(props.motDePasse)
+    let [theme, setTheme] = useState(props.theme)
+    let [photoDeProfil, setPhotoDeProfil] = useState(props.photoDeProfil)
+    let [contacts, setContacts] = useState(props.contacts)
 
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Mon nom est ${prenom} ${nom} ${courriel}`)
+    }
 
-    return <h2> mon nom est {prenom} {nom} </h2>
-    
+    let handleChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+        if (name === "nom") {
+            setNom(value)
+        } else if (name === "prenom") {
+            setPrenom(value)
+        } else if (name === "courriel") {
+            setCourriel(value)
+        }
+    }
+    return (
+        <>
+            <h1> Creer votre compte </h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Prenom : 
+                    <input
+                        type="text"
+                        name="prenom" 
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" onClick={() => handleSubmit} />
+                <label>
+                    Nom : 
+                    <input
+                        type="text"
+                        name="nom" 
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" onClick={() => handleSubmit} />
+                <label>
+                    Courriel : 
+                    <input
+                        type="text"
+                        name="courriel" 
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" onClick={() => handleSubmit} />
+            </form>
+        </>
+    )
+
 
 }
 
--- dev/PlanMe/src/main.jsx
+++ dev/PlanMe/src/main.jsx
@@ -7,8 +7,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Utilisateur prenom="finn" nom="kitty" />
+    {/* <App /> */}
+    <Utilisateur prenom="finn" nom="kitty" courriel="courriel" />
   </React.StrictMode>,
 )
 
