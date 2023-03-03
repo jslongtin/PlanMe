9483c3ce12517b1bb32e5827c3f8a826100e9d2a
Finnland <91551990+Finnlandish@users.noreply.github.com>
2023-03-03 12:54:45 -0500

    Revert "Merge pull request #2 from jslongtin/test"
    This reverts commit d8bf22332bb3bf1a843ca074bfefc1f3e4120bba, reversing
    changes made to b930c736b140e944c5596d2216e6b5981c52c2cb.

--- dev/PlanMe/src/App.jsx
+++ dev/PlanMe/src/App.jsx
@@ -3,32 +3,9 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Send the data to the server
-    fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        // Add logic to handle the server response here
-      })
-      .catch(error => console.error(error));
-  }
   const [count, setCount] = useState(0)
-  
+
   return (
-    
     <div className="App">
       
       <div className="card">
@@ -42,25 +19,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-      <button type="submit">Log in</button>
-    </form>
     </div>
   )
 }
--- dev/PlanMe/src/main.jsx
+++ dev/PlanMe/src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Module from './Module'
-import Utilisateur from './Utilisateur'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Utilisateur prenom="finn" nom="kitty" />
   </React.StrictMode>,
 )
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Module />);
