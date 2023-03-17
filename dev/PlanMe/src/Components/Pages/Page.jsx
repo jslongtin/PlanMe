5b3d95c44a59957ecaa7e665473ec4b560706159
Jess longtin <jessikalongtin@hotmail.com>
2023-03-17 16:47:57 -0400

    add ref Components dans Components

--- dev/PlanMe/src/Components/Pages/Page.jsx
+++ dev/PlanMe/src/Components/Pages/Page.jsx
@@ -5,6 +5,7 @@ import Menubar from '../MenuBar/Menubar'
 function Page() {
     return (
         // objet menubar dans la page 
+        // ref: w3school , page des components 
         <div><Menubar /></div>
 
     )
