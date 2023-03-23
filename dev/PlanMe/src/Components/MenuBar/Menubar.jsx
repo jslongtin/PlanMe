39168232d7b0e7e0ba66f2d7e579d07a0ae6a8d8
Jess longtin <jessikalongtin@hotmail.com>
2023-03-23 16:25:39 -0400

    ajout pdp dans barre avec nom d'utilisateur, reste a aller chercher nom dans la bd

--- dev/PlanMe/src/Components/MenuBar/Menubar.jsx
+++ dev/PlanMe/src/Components/MenuBar/Menubar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './MenuBar.css'
+import PhotoDeProfil from '../photoDeProfil/photoDeProfil'
 
 function Menubar() {
     return (
         <div id="menuBar">
             <div id="menuTopSection">
-                <div className="pdp"></div>
+                <div className="pdp"> <PhotoDeProfil /> </div>
+                {/* TODO */}
+                {/* allet chercher le nom de l'utilisateeur */}
+                <div id="navBarUsername">hello Kitty's Plan Me</div>
                 <div className="search"></div>
             </div>
             <div id="menuPages"></div>
---
+++ dev/PlanMe/src/Components/photoDeProfil/photoDeProfil.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './photoDeProfil.css'
+export class PhotoDeProfil extends Component {
+  render() {
+    return (
+     <div id="pdp"></div>
+    )
+  }
+}
+
+export default PhotoDeProfil
