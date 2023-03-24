fec37a7eb1746a4382950dd6c152867c977d9407
jslongtin <Jessikalongtin@hotmail.com>
2023-03-24 10:05:15 -0400

    changement icone placeholder plus debut de section page dans navbar

--- dev/PlanMe/src/Components/Navbar/navbar.jsx
+++ dev/PlanMe/src/Components/Navbar/navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import './MenuBar.css'
+import './navbar.css'
 import PhotoDeProfil from '../photoDeProfil/photoDeProfil'
+import { useState } from 'react'
+function Navbar(props) {
+    // REVIEW
+    let id = props.id
+    let titre = props.titre
+    let icone = props.icone
 
-function Menubar() {
+    let addPage = (page) => {
+        let section = <div className="pageSection"><div className="iconePageSection"></div> {page.icone} </div>
+    }
+
     return (
         <div id="menuBar">
             <div id="menuTopSection">
@@ -17,4 +26,4 @@ function Menubar() {
     )
 }
 
-export default Menubar
+export default Navbar
