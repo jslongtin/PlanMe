5a48f128a61787198d9581d3f3d39e819daa677a
Finnlandish <simpson.finnegan@gmail.com>
2023-04-02 22:22:34 -0400

    .

---
+++ dev/PlanMe/Docker/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
