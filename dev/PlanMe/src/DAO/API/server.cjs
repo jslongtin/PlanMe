1efa135ddd4d02a1a120ed2d10cd63aae49f426f
Finnlandish <simpson.finnegan@gmail.com>
2023-04-10 18:59:47 -0400

    ajout modif et delet user + file avec info pour le pool de BD TOTEST

--- dev/PlanMe/src/DAO/API/server.cjs
+++ dev/PlanMe/src/DAO/API/server.cjs
@@ -5,26 +5,45 @@ const bcrypt = require("bcrypt")
 const { sign } = require('jsonwebtoken');
 const pg = require('pg');
 const cors = require('cors');
-
+import { DB_HOST, DB_NAME, DB_USER, DB_PASS,DB_PORT } from './ConnectionDB.js';
 const app = express();
-app.use(bodyParser.json());
 
+// pool connection pour la base de donn
+const pool = new pg.Pool({
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASS,
+  port: DB_PORT,
+});
+
+//ref: chat gpt - https://www.youtube.com/watch?v=7nafaH9SddU
 app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, contenu-Type, Accept');
   next();
 });
-// Set up a connection to the PostgreSQL database using pg.Pool
-const pool = new pg.Pool({
-  host: 'localhost',
-  user: 'postgres',
-  database: 'postgres',
-  password: 'AAAaaa111',
-  port: 5432, // or your database's port
-});
+
+const userKey = generateKey(32);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: { userKey },
+  resave: false,
+  saveUninitialized: true,
+}));
+// Middleware pour vérifier si l'utilisateur est authentifié 
+// function authenticateUser - ref: chatgpt
+function authenticateUser(req, res, next) {
+  if (!req.session.userId) {
+    res.status(401).send('Non autorisé à accéder à cette ressource');
+    return;
+  }
+  next();
+}
 // enregistre un nouvel utilisateur et l'ajoute à la base de données
-app.post('/api/users', async (req, res) => {
+app.post('/api/register', async (req, res) => {
   try {
     
     const { email, username, password } = req.body;
@@ -46,6 +65,59 @@ app.post('/api/users', async (req, res) => {
     res.status(500).send('An error occurred while getting all users from the database.');
   }
 });
+// DELETE USER AND REDIRECT TO LOGIN PAGE
+app.post('/api/del_user', authenticateUser, async (req, res) => {
+  try {
+    const { email } = req.body;
+    const delUserQuery = 'DELETE FROM utilisateurs WHERE email = $1';
+    if(req.session.user && req.session.user.email === email){
+
+      await pool.query(delUserQuery, [email]);
+      console.log('User deleted:', { email });
+      req.session.destroy();
+      res.redirect('/login');
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    console.log('Error during deletion:', err.message);
+    console.error(err);
+    res.status(500).send('An error occurred while trying to delete user from the database.');
+  }
+});
+
+// Modifier info utilisateur courrant:
+app.post('/api/update_user', authenticateUser, async (req, res) => {
+  try {
+    const userid = req.session.user.id;
+    const { email, username, password } = req.body;
+
+    const checkUserQuery = 'SELECT * FROM utilisateurs WHERE email = $1';
+    const { rows } = await pool.query(checkUserQuery, [userid]);
+    const user = rows.find((user) => user.id === userid);
+    if (!user) {
+      res.status(401).json({ msg: 'User does not exist' })
+      return;
+    }
+    if (user.id != req.session.user.id) {
+      res.status(401).json({ msg: 'Action non-autorisee' })
+      return;
+    }
+    const salt = await bcrypt.genSalt(10); //ref : https://www.makeuseof.com/nodejs-bcrypt-hash-verify-salt-password/
+    const hash = await bcrypt.hash(password, salt);
+    const updateUserQuery = 'UPDATE utilisateurs SET email = $1, username = $2, password = $3 WHERE id = $4';
+    await pool.query(updateUserQuery, [email, username, hash, userid]);
+    console.log('User updated:', { email, username});
+    res.status(200).json(user);
+  } catch (err) {
+    console.log('Error during updating user:', err.message);
+    console.error(err);
+    res.status(500).send('An error occurred while trying to update user from the database.');
+  }
+});
+
+
+
+
 // vérifie si l'utilisateur existe dans la base de données
 app.post('/api/login', async (req, res) => {
   try {
@@ -64,7 +136,7 @@ app.post('/api/login', async (req, res) => {
         }
         
     }
-    
+    // """TOKEN GENERATION""" TODO
     // const payload = {
     //   user: {
     //     id: user.id,
@@ -81,6 +153,7 @@ app.post('/api/login', async (req, res) => {
     //     res.json({ token });
     //   }
     // );    
+
   } catch (err) {
     console.log('Error during getting all users:', err.message);
     console.error(err);
@@ -110,4 +183,11 @@ app.listen(port, () => {
 });
 
 
+// ref: https://www.tabnine.com/code/javascript/functions/crypto/randomBytes
+const crypto = require('crypto');
 
+const generateKey = (length) => {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
