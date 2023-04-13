67c2f5c56a7b89ebae83f5b8619a9cf77ab30768
jslongtin <Jessikalongtin@hotmail.com>
2023-04-13 15:29:08 -0400

    add boutons add et mod , a terminer

---
+++ dev/PlanMe/src/Components/modules/addMod/addMod.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './addMod.css'
+
+function AddMod() {
+  return (
+    <div className='addMod'>
+        <button><img className='plus' src="..\..\..\src\assets\images\plus.png" alt="plus" /></button>
+        <button><img className='mod' src="..\..\..\src\assets\images\mod.png" alt="plus" /></button>
+ </div>
+  )
+}
+
+export default AddMod
