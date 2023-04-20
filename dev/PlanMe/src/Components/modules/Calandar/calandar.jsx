7c43d16e51c0340a1c84fb446c95b72deeeb436a
Finnland <91551990+Finnlandish@users.noreply.github.com>
2023-04-20 14:03:48 -0400

    ref de full calandar added

--- dev/PlanMe/src/Components/modules/Calandar/calandar.jsx
+++ dev/PlanMe/src/Components/modules/Calandar/calandar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 //ref : https://www.npmjs.com/package/react-calendar
+//ref : https://fullcalendar.io/docs/react
 function Calendrier() {
   const [value, onChange] = useState(new Date());
 
