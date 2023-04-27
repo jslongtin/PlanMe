97e56920ef2d34699cc4448f57b9ae055d3dec7d
Finnland <91551990+Finnlandish@users.noreply.github.com>
2023-04-27 11:45:12 -0400

    tailwind prefab for login n register

--- dev/PlanMe/src/Components/Login/Login.jsx
+++ dev/PlanMe/src/Components/Login/Login.jsx
@@ -4,7 +4,7 @@ import "./login.css";
 const Login = ({ onPageChange }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // ref : open ai
+
   const handleLogin = async (e) => {
     e.preventDefault();
     const response = await fetch("http://127.0.0.1:3001/api/login", {
@@ -25,32 +25,54 @@ const Login = ({ onPageChange }) => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-        <button onClick={() => onPageChange("register")}>register</button>
-      </form>
+    <div className="flex items-center justify-center h-screen bg-primaryColor">
+      <div className="bg-secondaryColor p-10 rounded-lg shadow-2xl w-96">
+        <h1 className="text-3xl font-bold mb-10 text-center text-primaryColor">Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="mb-5">
+            <label htmlFor="email" className="block mb-2 text-sm font-bold text-primaryColor">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="block w-full p-3 rounded-lg bg-white border border-primaryColor focus:outline-none focus:border-thirdColor"
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="password" className="block mb-2 text-sm font-bold text-primaryColor">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="block w-full p-3 rounded-lg bg-white border border-primaryColor focus:outline-none focus:border-thirdColor"
+            />
+          </div>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="py-3 px-6 bg-primaryColor text-green font-bold rounded-full hover:bg-ash focus:outline-none focus:bg-thirdColor"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="text-center mt-8">
+          <button
+            onClick={() => onPageChange("register")}
+            className="text-primaryColor font-semibold hover:text-thirdColor focus:outline-none"
+          >
+            Don't have an account? Register
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
