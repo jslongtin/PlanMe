5db7caf9df726c82ea5c6948d11a6b13c8b8bde3
Finnlandish <simpson.finnegan@gmail.com>
2023-05-17 18:47:37 -0400

    page undefined

--- dev/PlanMe/src/Components/dashboard/dashboard.jsx
+++ dev/PlanMe/src/Components/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "../Navbar/navbar";
 import Page from "../Pages/page";
 import "./dashboard.css";
+import { Route } from "react-router-dom";
 
 function Dashboard() {
   return (
@@ -11,7 +12,7 @@ function Dashboard() {
       </div>
 
       <div className="h-screen" id="pageContainer">
-        <Page />
+        <Route path="/dashboard/:pageId" component={Page} />
       </div>
     </div>
   );
