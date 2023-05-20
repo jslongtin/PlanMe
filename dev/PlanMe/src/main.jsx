acdda9987b689c04b01bb7fa3cbc75814f5011ad
Finnlandish <simpson.finnegan@gmail.com>
2023-05-20 14:23:29 -0400

    home page

--- dev/PlanMe/src/main.jsx
+++ dev/PlanMe/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import Home from "./Components/home/Home";
 import Register from "./Components/register/Register";
 import Navbar from "./Components/Navbar/navbar";
 import Dashboard from "./Components/dashboard/dashboard";
@@ -14,7 +15,8 @@ const App = () => {
   return (
     <div id="main">
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
         <Route path="/profile" component={Utilisateur} />
         <Route path="/register" component={Register} />
         <Route path="/dashboard" component={Dashboard} />
